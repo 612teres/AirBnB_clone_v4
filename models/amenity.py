4c3d458bcd75dd32b4b00598820e2b0aa2a4bf5a
Fabian Teres <612teres@gmail.com>
2025-10-06 11:58:01 +0300

    Update docstrings for Amenity class

--- models/amenity.py
+++ models/amenity.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-""" holds class Amenity"""
+""" holds the amenity class"""
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
@@ -9,7 +9,7 @@ from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """Representation of Amenity """
+    """Representation of Amenities in the db """
     if models.storage_t == 'db':
         __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
